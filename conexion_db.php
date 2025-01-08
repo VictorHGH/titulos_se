4adadbf1cf02893dcaefef65584908bff64a390b
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-01-08 12:12:31 -0600

    [New] file for unic connection

---
+++ conexion_db.php
@@ -0,0 +1,9 @@
+<?php
+
+// Conexión a la base de datos (asumiendo que ya tienes una conexión establecida)
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "titulos_se";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
