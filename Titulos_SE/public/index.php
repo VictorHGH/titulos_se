c2f29b71cdf8fff7656e8941939a082efa43c7f9
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-03-13 22:33:21 -0600

    [Updated] index for not relative paths

--- Titulos_SE/public/index.php
+++ Titulos_SE/public/index.php
@@ -1,6 +1,4 @@
 <?php
-
-require_once '../config/database.php';
-require_once '../App/Core/autoload.php';
-
-require_once '../App/Domains/User/Routes/web.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../App/Core/autoload.php';
+require_once __DIR__ . '/../App/Routes/web.php';
