e3171358256f76aa559a4ce44a38b4dc48639d80
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-06-10 12:37:19 -0600

    [Updated] index.php

--- Titulos_SE/public/index.php
+++ Titulos_SE/public/index.php
@@ -1,4 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../App/Core/autoload.php';
-require_once __DIR__ . '/../App/Routes/web.php';
+echo "Hola mundo";
+/* require_once __DIR__ . '/../config/database.php'; */
+/* require_once __DIR__ . '/../App/Core/autoload.php'; */
+/* require_once __DIR__ . '/../App/Routes/web.php'; */
